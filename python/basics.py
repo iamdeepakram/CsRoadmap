a9498b1e48e398c11abcd4674a1b3f0f1a946a69
iamdeepakram <deepaksutrakar@aol.com>
2020-12-11 16:57:21 +0530

    added basics py file

---
+++ python/basics.py
@@ -0,0 +1,58 @@
+# Exploring pythn interactive shell 
+""" +, - , /, *
+
+2 + 2
+
+
+
+ """ 
+# Math operators from higher to lowest presedence 
+
+# Data Types
+# integer, float, string
+
+"""
+Mostly common Data types
+
+Data type            | Examples
+Integer              | 34, 2, 1, 7, 4
+Float point numnbers | 5.6, 12.0, 7.8, 3.4
+
+
+We can use math operators on string too.
+
+String concetnation
+
+'praveen' + 'is here'
+'praveen'*3
+--> 'praveen'
+--> * 
+--> 3
+--> 'praveen' 'praveen' 'praveen'
+--> 'praveen'+'praveen'+'praveen'
+--> 'praveenpraveenpraveen'
+
+"""
+# Assigning values to variables
+"""
+Assignment statement
+test = 22
+
+"""
+test = 22
+def addsomething():
+    test = 11
+    return print(test)
+addsomething()
+print('----------------------')
+
+## ask your name program 
+
+# ask for name via question
+print('What is your name?')
+# user can input there name
+username = input()
+if username == '':
+    print('Please enter user name!')
+# print greeting with username
+print('Nice to meet you '+ username + '!')
